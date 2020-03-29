ad528b83101a987eb950a7be41fe59d22e7832b2
Taimoor Raza <taimoor.raza7@gmail.com>
2020-03-30 00:21:59 +0500

    Initial basic setup

---
+++ e-commerce/src/components/homepage.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtitle">Sub Title</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtitle">Sub Title</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtitle">Sub Title</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
