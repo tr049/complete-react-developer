bbb3166dbbd816528308198757fc303d8ef0ef76
Taimoor Raza <taimoor.raza7@gmail.com>
2020-03-31 02:51:07 +0500

    Components added
    Header, Item collection, item preview etc components added, basic routing added

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/shop" component={ShopPage}/>
+        </Switch>
+      </BrowserRouter>
     </div>  
   );
 }
