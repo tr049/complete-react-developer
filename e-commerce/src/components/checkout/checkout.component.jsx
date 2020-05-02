8d34d4f6e5906da353dcfb7e54ab5ad57e3b06ff
Taimoor Raza <taimoor.raza7@gmail.com>
2020-05-03 03:01:51 +0500

    Stripe integrated and styling
    Stripe integrated and favicon added and updated styles

--- e-commerce/src/components/checkout/checkout.component.jsx
+++ e-commerce/src/components/checkout/checkout.component.jsx
@@ -3,8 +3,10 @@ import {createStructuredSelector} from "reselect";
 import {connect} from "react-redux";
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selector";
 import CheckoutItem from "../checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
+import StripeCheckout from "../stripe-button/stripe-button.component";
 
 
 const CheckoutPage = ({cartItems, total}) => (
@@ -34,6 +36,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/21 CVV: 123
+        </div>
+        <StripeCheckout price={total} />
     </div>
 );
 
